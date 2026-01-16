65f79b4ca4e891f39c218703f801956d88ac545c
tyloner <wofa11@gmail.com>
2026-01-16 14:44:05 +0100

    Delete app/contact/page.tsx

--- app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ContactPage() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Contact</h1>
-      <p>Temporary page to unblock deployment.</p>
-    </main>
-  );
-}
